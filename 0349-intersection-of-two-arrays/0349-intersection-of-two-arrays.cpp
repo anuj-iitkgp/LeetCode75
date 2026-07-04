d8d32dc7b760c12070fe2c6d3bc19999da4f9bf0
Anuj Yadav <anujyiitk@gmail.com>
2026-07-05 00:20:23 +0530

    Time: 0 ms (100%), Space: 14.6 MB (16.23%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        set<int> ans;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(nums1[i] == nums2[j]){
-                    ans.insert(nums1[i]);
-                }
+        unordered_set<int> seen(nums1.begin(), nums1.end());
+        unordered_set<int> ans;
+
+        for(auto num : nums2){
+            if(seen.count(num)){
+                ans.insert(num);
             }
         }
+        
+        
        return vector<int>(ans.begin(), ans.end());
     }
 };
