cf8dfda067d10fc225b2872bb207fc0351fa6eaa
Anuj Yadav <anujyiitk@gmail.com>
2026-07-20 15:31:20 +0530

    Time: 7 ms (75.31%), Space: 17.8 MB (54.44%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            ans[i] = 1 + ans[i - offset]
+        return ans
+        
