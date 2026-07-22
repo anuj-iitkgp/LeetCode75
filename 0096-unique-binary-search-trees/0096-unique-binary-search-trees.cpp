6fc2f2ff1fd36b9880c5aec5845e29eed162921b
Anuj Yadav <anujyiitk@gmail.com>
2026-07-22 22:57:21 +0530

    Time: 1 ms (16.49%), Space: 8.2 MB (18.38%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -10,9 +10,25 @@ public:
         }
         return dp[n] = ans;
     }
+
+    int solveTab(int n){
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1;
+        // i represent no of nodes
+        // j represent as a root 
+        for(int i = 2; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+
     int numTrees(int n) {
         
-        vector<int> dp(n + 1, -1);
-        return solveMem(n, dp);
+       // vector<int> dp(n + 1, -1);
+        // return solveMem(n, dp);
+
+        return solveTab(n);
     }
 };
